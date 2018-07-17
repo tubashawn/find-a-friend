df3f7bda1c1865a62309c9947d27ba0413a05e6e
tubaShawn <shawnrmangum@gmail.com>
2018-07-16 21:45:22 -0600

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendsArray = [
+    {
+        "name": "Tito",
+        "photo": "http://www.titogoldstein.com/wp-content/uploads/2014/06/Screen-Shot-2014-06-27-at-4.26.32-PM.png",
+        "scores": [
+            1,
+            2,
+            3,
+            4,
+            5,
+            4,
+            3,
+            2,
+            1,
+            5
+        ]
+    }
+];
+
+module.exports = friendsArray;
